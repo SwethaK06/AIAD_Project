0997c3276e2a6c6c4a26da3a3266179b9add2b5d
roxyalt <shadowlai001@gmail.com>
2026-08-22 22:36:00 +0800

    updated

--- ML_Service/app.py
+++ ML_Service/app.py
@@ -1,5 +1,7 @@
 import os
 from fastapi import FastAPI, HTTPException
+from pathlib import Path
+from dotenv import load_dotenv
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import joblib
